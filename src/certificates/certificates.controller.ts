96dd97616156b196874ac5d677abfef25e87b094
Amirul Ikhwan <amirikhwanfaisal@gmail.com>
2026-04-23 01:16:49 +0800

    something

--- src/certificates/certificates.controller.ts
+++ src/certificates/certificates.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateCertificatesDto } from './dto/create-certifictes.dto';
 import { CertificatesService } from './certificates.service';
 
@@ -7,11 +7,16 @@ export class CertificatesController {
 
     constructor(private readonly certificateService : CertificatesService){}
 
+
+    @Get('health')
+    async getHealth(){
+        return 'Certificates Service is working';
+    }
+
     @Post('generate')
     async generate(
         @Body() data : CreateCertificatesDto
     ){
-
         return this.certificateService.generate(data.particpantName , data.templateName , data.config);
     }
 }
